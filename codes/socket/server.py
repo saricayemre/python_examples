03416887fd2a619f1be6ed0390cb38312e4d5ad2
Yunus Emre Sarıca <68511425+saricayemre@users.noreply.github.com>
2023-08-31 18:08:37 +0300

    Add files via upload

---
+++ codes/socket/server.py
@@ -0,0 +1,25 @@
+import  socket
+import  subprocess
+
+host = '127.0.0.1'
+port = 50002
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host,port))
+server_socket.listen()
+
+conn,addr =server_socket.accept()
+print("command from :"+str(addr))
+
+while True:
+    data = conn.recv(1024).decode()
+    print(data)
+
+    result = subprocess.run(data,stdout=subprocess.PIPE,shell=True)
+    if(result.stdout.decode()!=""):
+        response_data = result.stdout
+    else:
+        response_data = ("command executed").encode()
+
+    conn.send(response_data)
+conn.close()
